30850008b52ff3a3d63ce6339d6cc33b6a12f582
aleksey-saenko <aleksei.saenko.gm@gmail.com>
2023-09-26 19:13:24 +0300

    Updated dependencies, README.md.

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
 # TTL Changer
 
-A simple application that allows you to change the TTL (Time to Live) value. Features included:
+<a href="https://f-droid.org/en/packages/com.mrsep.ttlchanger/">
+  <img alt="Get it on F-Droid"
+       height="80"
+       src="https://f-droid.org/badge/get-it-on.png" />
+</a>
 
+A simple application that allows you to change the TTL (Time to Live) value. Features included:
 * Read-write TTL value
 * Enable IPv6 support
 * Apply last written value on device boot
--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,7 +50,7 @@ android {
 // Setup protobuf configuration, generating lite Java and Kotlin classes
 protobuf {
     protoc {
-        artifact = "com.google.protobuf:protoc:3.23.4"
+        artifact = "com.google.protobuf:protoc:3.24.0"
     }
     generateProtoTasks {
         all().forEach { task ->
@@ -67,32 +67,32 @@ protobuf {
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.10.1")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation(platform("org.jetbrains.kotlin:kotlin-bom:1.9.10"))
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.activity:activity-compose:1.7.2")
-    implementation(platform("androidx.compose:compose-bom:2023.08.00"))
+    implementation(platform("androidx.compose:compose-bom:2023.09.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
 
-    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.6.1")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.1")
+    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.6.2")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
     implementation("androidx.datastore:datastore:1.0.0")
-    implementation("com.google.protobuf:protobuf-kotlin-lite:3.23.4")
-    implementation("com.google.protobuf:protoc:3.23.4")
+    implementation("com.google.protobuf:protobuf-kotlin-lite:3.24.0")
+    implementation("com.google.protobuf:protoc:3.24.3")
 
     implementation("com.google.accompanist:accompanist-systemuicontroller:0.32.0")
 
-    implementation("androidx.glance:glance:1.0.0-rc01")
-    implementation("androidx.glance:glance-appwidget:1.0.0-rc01")
-    implementation("androidx.glance:glance-material3:1.0.0-rc01")
+    implementation("androidx.glance:glance:1.0.0")
+    implementation("androidx.glance:glance-appwidget:1.0.0")
+    implementation("androidx.glance:glance-material3:1.0.0")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.08.00"))
+    androidTestImplementation(platform("androidx.compose:compose-bom:2023.09.00"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
